69026b1e6f98c3588365db2a67a4551216d1d40d
Igor Santos <falaigors@gmail.com>
2022-06-28 10:45:47 -0300

    ignore _app.tsx in coverage test

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,7 @@ module.exports = {
   setupFilesAfterEnv: ["<rootDir>/src/tests_utils/setupTests.ts"],
   testEnvironment: "jsdom",
   collectCoverageFrom: ["<rootDir>/src/**/*.(ts|tsx)"],
+  coveragePathIgnorePatterns: ["<rootDir>/src/pages/_app.tsx"],
   transform: {
     "^.+\\.(ts|js|tsx|jsx)$": "ts-jest",
     "^.+\\.(ts|js|tsx|jsx)$": "@swc/jest",
