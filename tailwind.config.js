88eeef7ddb3cac9fe594c666cea95d11dfcaea39
Igor Santos <falaigors@gmail.com>
2022-08-10 09:38:17 -0300

    gradient color in text

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,9 +8,13 @@ module.exports = {
       },
       colors: {
         blue: "#0AA7FF",
+        'light-blue': "#2EE7E7",
         dark: "#181818",
         white: "#F3F1EF",
       },
+      dropShadow: {
+        'custom': '0 0 76px rgba(188, 251, 195, 0.30)',
+      }
     },
   },
   plugins: [],
