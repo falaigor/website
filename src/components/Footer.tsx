afb737bf58baae3158838308a2d975df5bb5d5c7
Igor Santos <falaigors@gmail.com>
2022-08-17 20:52:17 -0300

    update footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,20 +4,23 @@ import { GithubLogo, InstagramLogo, LinkedinLogo } from "phosphor-react";
 
 export const Footer = () => {
   return (
-    <footer className="bg-white p-8 flex" data-testid="footer">
+    <footer data-testid="footer" className="p-8 flex">
       <div className="flex max-w-7xl w-full m-auto justify-between items-center py-8 px-8 xl:px-0">
         <Image
-          src="/logo-dark.svg"
+          src="/logo-white.svg"
           alt="Logo Igor Santos"
           width="160px"
           height="30px"
         />
 
         <div className="flex gap-2 mt-4">
-          <Link href="https://github.com/falaigor/" target="_blank" passHref>
-            <span className="cursor-pointer bg-dark hover:bg-blue p-2 text-white hover:text-white text-2xl transition-all">
+          <Link href="https://github.com/falaigor/" passHref>
+            <a
+              target="_blank"
+              className="cursor-pointer border-2 border-dark-blue rounded-md hover:bg-dark-blue p-2 text-white text-2xl transition-all"
+            >
               <GithubLogo />
-            </span>
+            </a>
           </Link>
 
           <Link
@@ -25,9 +28,12 @@ export const Footer = () => {
             href="https://www.instagram.com/falaigors/"
             passHref
           >
-            <span className="cursor-pointer bg-dark hover:bg-blue p-2 text-white hover:text-white text-2xl transition-all">
+            <a
+              target="_blank"
+              className="cursor-pointer border-2 border-dark-blue rounded-md hover:bg-dark-blue p-2 text-white text-2xl transition-all"
+            >
               <InstagramLogo />
-            </span>
+            </a>
           </Link>
 
           <Link
@@ -35,9 +41,12 @@ export const Footer = () => {
             href="https://www.linkedin.com/in/falaigor/"
             passHref
           >
-            <span className="cursor-pointer bg-dark hover:bg-blue p-2 text-white hover:text-white text-2xl transition-all">
+            <a
+              target="_blank"
+              className="cursor-pointer border-2 border-dark-blue rounded-md hover:bg-dark-blue p-2 text-white text-2xl transition-all"
+            >
               <LinkedinLogo />
-            </span>
+            </a>
           </Link>
         </div>
       </div>
