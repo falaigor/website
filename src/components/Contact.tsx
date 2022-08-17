83daa938e2b66457e3c84e4a2697a77d1bfc1006
Igor Santos <falaigors@gmail.com>
2022-08-17 00:21:32 -0300

    new formact contact page

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,30 +1,29 @@
 import Link from "next/link";
+import { Chat } from "phosphor-react";
 
 export const Contact = () => {
   return (
-    <section
-      data-testid="contact"
-      id="contact"
-      className="max-w-6xl m-auto my-24 px-8 xl:px-0"
-    >
-      <h2 className="font-Blenny text-5xl ">Fancy a chat?</h2>
+    <section data-testid="contact" id="contact" className="bg-light-dark">
+      <div className="max-w-6xl m-auto py-24 text-center flex flex-col gap-4">
+        <h2 className="font-Jost font-bold text-5xl text-blue drop-shadow-custom shadow-red-400/100">
+          Fancy a chat?
+        </h2>
 
-      <p className="text-xl max-w-2xl">
-        I’d love to connect — let’s have a cup of coffee, whether it’s for your
-        exciting new project, an upcoming talk or just for fun. Feel free to
-        send me an{" "}
-        <Link href="/as">
-          <span className="cursor-pointer underline underline-offset-2">
-            email
-          </span>
-        </Link>{" "}
-        or{" "}
-        <Link href="/as">
-          <span className="cursor-pointer underline underline-offset-2">
-            reach out
-          </span>
+        <p className="text-lg max-w-2xl m-auto">
+          Let’s have a conversation! I’d love to hear about what you’re working
+          on and find a way to work together.
+        </p>
+
+        <Link href="https://www.linkedin.com/in/falaigor/" passHref>
+          <a
+            data-testid="chat-button"
+            target="_blank"
+            className="w-32 m-auto flex justify-between items-center py-2 px-6 text-zinc-300 border-2 border-dark-blue rounded-lg hover:text-zinc-100 hover:bg-dark-blue text-lg transition-all"
+          >
+            <Chat size={24} /> Chat
+          </a>
         </Link>
-      </p>
+      </div>
     </section>
   );
 };
--- src/components/__tests__/Contact.test.tsx
+++ src/components/__tests__/Contact.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { Contact } from "../Contact";
 
 describe("Contact", () => {
@@ -6,5 +6,15 @@ describe("Contact", () => {
     render(<Contact />);
 
     expect(screen.getByTestId("contact")).toBeInTheDocument();
+  });
+
+  it("should redirect to Github when click button", async () => {
+    render(<Contact />);
+
+    const button = screen.getByTestId("chat-button");
+    await fireEvent.click(button);
+
+    window.location.assign(button.href);
+    expect(window.location).toBeAt(button.href);
   });
 });
