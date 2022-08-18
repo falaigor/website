eb715be5b9eeaefa92bf4c1f0f6c1921512cb34e
Igor Santos <falaigors@gmail.com>
2022-08-17 23:53:07 -0300

    update hero component

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,7 +2,7 @@ export const Hero = () => {
   return (
     <div data-testid="hero" className="max-w-7xl m-auto">
       <div className="flex flex-col mt-14 p-6 gap-5 max-w-4xl">
-        <h1 className="text-7xl font-Jost font-bold bg-clip-text text-transparent bg-gradient-to-b from-blue to-light-blue drop-shadow-custom shadow-red-400/100">
+        <h1 className="text-5xl sm:text-7xl font-Jost font-bold bg-clip-text text-transparent bg-gradient-to-b from-blue to-light-blue drop-shadow-custom shadow-red-400/100">
           Eu sou Igor Santos, desenvolvedor front-end.
         </h1>
         <p className="text-base">
