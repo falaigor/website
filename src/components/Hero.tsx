3e475003240c85946cf8239ee79d3cce2217c7b6
Igor Santos <falaigors@gmail.com>
2022-11-24 00:26:23 -0300

    new font hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,9 +1,11 @@
 export function Hero() {
   return (
     <div data-testid="hero" className="max-w-7xl m-auto">
-      <div className="flex flex-col mt-14 p-6 gap-5 max-w-4xl">
-        <h1 className="text-5xl sm:text-7xl font-Jost font-bold bg-clip-text text-transparent bg-gradient-to-b from-blue to-light-blue drop-shadow-custom shadow-red-400/100">
-          Eu sou Igor Santos, desenvolvedor front-end.
+      <div className="flex flex-col mt-14 p-6 gap-5">
+        <h1 className="text-5xl sm:text-7xl font-Miligram font-bold bg-clip-text text-transparent bg-gradient-to-b from-blue to-light-blue drop-shadow-custom shadow-red-400/100">
+          Olá! Eu sou
+          <br /> Igor Santos, <br />
+          Desenvolvedor FullStack.
         </h1>
         <p className="text-base">
           Sou especializado no desenvolvimento de website e aplicações web.
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,25 @@
 module.exports = {
-  content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
-  theme: {
-    extend: {
-      fontFamily: {
-        Jost: ["Jost", "sans-serif"],
-        JetBrains: ["JetBrains Mono", "monospace"],
-      },
-      colors: {
-        blue: "#0AA7FF",
-        'light-blue': "#2EE7E7",
-        'dark-blue': "#7CC9FF41",
-        dark: "#181818",
-        'light-dark': '#1C2C35',
-        'header': '#1C2A34',
-        white: "#F3F1EF",
-      },
-      dropShadow: {
-        'custom': '0 0 76px rgba(188, 251, 195, 0.30)',
-      }
+    content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
+    theme: {
+        extend: {
+            fontFamily: {
+                Jost: ["Jost", "sans-serif"],
+                Miligram: ["Miligram", "sans-serif"],
+                JetBrains: ["JetBrains Mono", "monospace"],
+            },
+            colors: {
+                blue: "#0AA7FF",
+                "light-blue": "#2EE7E7",
+                "dark-blue": "#7CC9FF41",
+                dark: "#181818",
+                "light-dark": "#1C2C35",
+                header: "#1C2A34",
+                white: "#F3F1EF",
+            },
+            dropShadow: {
+                custom: "0 0 76px rgba(188, 251, 195, 0.30)",
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 };
