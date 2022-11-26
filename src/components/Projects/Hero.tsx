0db5c73c4866348d095134b8d6fd693d665f21f0
Igor Santos <falaigors@gmail.com>
2022-11-26 00:35:14 -0300

    update title projects page

--- src/components/Projects/Hero.tsx
+++ src/components/Projects/Hero.tsx
@@ -3,7 +3,7 @@ export function HeroProjects() {
     <div data-testid="hero" className="max-w-7xl m-auto">
       <div className="flex flex-col mt-14 p-6">
         <h1 className="text-5xl sm:text-7xl font-Miligram font-bold bg-clip-text text-transparent bg-gradient-to-b from-blue to-light-blue drop-shadow-custom shadow-red-400/100">
-          Veja os projetos <br /> que desenvolvi.
+          Alguns projetos <br /> desenvolvidos.
         </h1>
       </div>
     </div>
